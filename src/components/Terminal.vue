29cf2c2f7140896892336924124bce414cd445b5
asuis <727443530@qq.com>
2020-02-06 15:31:03 +0800

    create Terminal components

---
+++ src/components/Terminal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <div id="term" ref="terminal"></div>
+  </div>
+</template>
+
+<script>
+
+import {Terminal} from 'xterm'
+import 'xterm/css/xterm.css'
+
+export default {
+
+    name: 'Terminal',
+    mounted() {
+        var term = new Terminal()
+        term.open(this.$refs.terminal)
+        term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+    }
+}
+</script>
+
+<style>
+    #term{
+        width: 600px;
+        height: 400px;
+    }
+</style>
